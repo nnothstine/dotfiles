550561d60b6a34d65a7e0dc7035ec485adba0623
Niklas Nothstine <niklas@nothstine.io>
2020-10-02 09:59:02 -0500

    minor tweaks v2

--- dev/python/env.zsh
+++ dev/python/env.zsh
@@ -2,5 +2,6 @@ export PYENV_ROOT="$XDG_DATA_HOME/pyenv"
 export PYTHONHISTORY="$XDG_CACHE_HOME/pythonhistory"
 path=( $PYENV_ROOT/shims $path )
 
-#__nan_has 'pyenv' && eval "$(pyenv init -)"
-#__nan_has 'pipenv' && eval "$(pipenv --completion)"
+__nan_has 'pyenv' && eval "$(pyenv init -)"
+__nan_has 'pyenv-virtualenv' && eval "$(pyenv virtualenv-init -)"
+# __nan_has 'pipenv' && eval "$(pipenv --completion)"
