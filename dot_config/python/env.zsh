a9c89a95bfa533d964e170f683bfc005d4a09876
Niklas Nothstine <niklas@nothstine.io>
2021-04-11 16:31:09 -0500

    Language environments

--- dot_config/python/env.zsh
+++
@@ -1,6 +0,0 @@
-export PYENV_ROOT="$XDG_DATA_HOME/pyenv"
-export PYTHONHISTORY="$XDG_CACHE_HOME/pythonhistory"
-path=($PYENV_ROOT/shims $path)
-
-__nan_has 'pyenv' && eval "$(pyenv init -)"
-__nan_has 'pyenv-virtualenv' && eval "$(pyenv virtualenv-init -)"
